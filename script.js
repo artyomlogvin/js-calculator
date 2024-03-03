ff443336c52e175df1768ee0d0d5dcf6ca493307
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-03 15:55:19 +0300

    Add percentage operation

--- script.js
+++ script.js
@@ -15,18 +15,27 @@ function divide(a, b) {
     return Math.round(a / b) / 10;
 }
 
+function dividePercentage(a) {
+    return a / 100;
+}
+
 // Operators reference 
 const operators = {
     '+': add,
     '-': subtract,
     '*': multiply,
     '/': divide,
+    '%': dividePercentage,
 };
 
 // Calling operations
-function operate(operator, numFirst, numSecond) {
+function operate(operator, numFirst, numSecond = null) {
     if (operator in operators) {
-        return operators[operator](numFirst, numSecond);
+        if (operator === '%') {
+            return operators['%'](numFirst);
+        } else { 
+            return operators[operator](numFirst, numSecond) 
+        };
     } else {
         return 'No such operator!';
     }
@@ -35,3 +44,6 @@ function operate(operator, numFirst, numSecond) {
 let firstNumber = 0;
 let operator = '';
 let secondNumber = 0;
+
+let currentPrompt = [];
+
