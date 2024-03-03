850e2e38ec73add920a9c576563ab2f9db0989a8
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-03 15:22:13 +0300

    Add function operate

--- script.js
+++ script.js
@@ -11,9 +11,29 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return Math.round(a / b) / 10;
+}
+
+const operators = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+};
+
+function operate(operator, numFirst, numSecond) {
+    if (operator in operators) {
+        return operators[operator](numFirst, numSecond);
+    } else {
+        return 'No such operator!';
+    }
 }
 
 let firstNumber = 0;
 let operator = '';
 let secondNumber = 0;
+
+console.log(operate('+', 2, 3));
+console.log(operate('-', 2, 3));
+console.log(operate('*', 2, 3));
+console.log(operate('/', 2, 3));
